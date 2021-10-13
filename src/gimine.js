4d4c4e13f9ce026a0666f3e16ca44d1eda346d4e
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-10-13 20:52:16 +0300

    Initial commit

---
+++ src/gimine.js
@@ -0,0 +1,16 @@
+const gimine = [
+    {
+        name: 'Petras',
+        age: 99,
+    },
+    {
+        name: 'Maryte',
+        age: 103,
+    },
+    {
+        name: 'Jonas',
+        age: 50,
+    },
+];
+
+module.exports = gimine;
---
+++ src/kasVyriausias.js
@@ -0,0 +1,18 @@
+/**
+ * Skirta rasti ilgiausiai gimineje isgyvenusi asmeni
+ * @param {Object[]} giminesMedis Gimines medi aprasantis objektas
+ * @returns {number} Ilgiausiai gyvenusio asmens amzius
+ */
+ function kasVyriausias(giminesMedis) {
+    let maxAge = 0;
+    giminesMedis.forEach(element => {
+        if (element.age > maxAge) {
+           maxAge = element.age;
+        }
+    });
+
+    // console.log(giminesMedis);
+    return maxAge;
+}
+
+module.exports = kasVyriausias;
